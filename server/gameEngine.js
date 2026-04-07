c7a5d5d6ba6dd819d60b82ffe047bee5698820a5
himanshu14-supreme <himanshuhadbadi14@gmail.com>
2026-04-07 14:05:52 +0530

    Update gameEngine.js

--- server/gameEngine.js
+++ server/gameEngine.js
@@ -1,1 +1,25 @@
+// server/gameEngine.js
 
+function movePlayer(player, roll) {
+    if (player.pos === -1) {
+        if (roll === 6) player.pos = 0;
+        return;
+    }
+
+    player.pos += roll;
+
+    if (player.pos > 56) {
+        player.pos = 56;
+    }
+}
+
+function checkCollision(players, currentPlayer) {
+    return players.find(
+        p => p.id !== currentPlayer.id &&
+        p.pos === currentPlayer.pos &&
+        p.pos !== -1 &&
+        p.pos !== 56
+    );
+}
+
+module.exports = { movePlayer, checkCollision };
