e2682ff364dec4e42cd8f246161149de2628f45e
himanshu14-supreme <himanshuhadbadi14@gmail.com>
2026-04-07 14:07:53 +0530

    Update socket.js

--- public/socket.js
+++ public/socket.js
@@ -1,1 +1,14 @@
+// public/socket.js
 
+import { updatePlayers } from './ui.js';
+
+const socket = io();
+
+export function setupSocket() {
+
+    socket.on('diceRolled', (data) => {
+        updatePlayers(data.players);
+    });
+
+    return socket;
+}
