45e0b1f97db2ef185667355fed343e4a101a9a94
himanshu14-supreme <himanshuhadbadi14@gmail.com>
2026-04-07 14:07:33 +0530

    Update ui.js

--- public/ui.js
+++ public/ui.js
@@ -1,1 +1,6 @@
+// public/ui.js
 
+export function updatePlayers(players) {
+    document.getElementById('players').innerHTML =
+        players.map(p => `<div>${p.name} (${p.pos})</div>`).join('');
+}
