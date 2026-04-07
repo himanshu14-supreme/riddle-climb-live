5fd5d9b74be57712399f7056e90fd6dad51d9a76
himanshu14-supreme <himanshuhadbadi14@gmail.com>
2026-04-07 14:08:14 +0530

    Update main.js

--- public/main.js
+++ public/main.js
@@ -1,1 +1,16 @@
+// public/main.js
 
+import { buildBoard } from './game.js';
+import { setupSocket } from './socket.js';
+
+const socket = setupSocket();
+
+window.start = () => {
+    socket.emit('createRoom');
+};
+
+window.roll = () => {
+    socket.emit('rollDice');
+};
+
+buildBoard();
