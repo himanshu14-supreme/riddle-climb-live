b71f3d1d866409b3c975db243ef68c992fbad8ba
himanshu14-supreme <himanshuhadbadi14@gmail.com>
2026-04-07 14:06:20 +0530

    Update roomManager.js

--- server/roomManager.js
+++ server/roomManager.js
@@ -1,1 +1,32 @@
+// server/roomManager.js
 
+const rooms = {};
+
+function createRoom(socket) {
+    const id = Math.random().toString(36).substring(2, 6).toUpperCase();
+
+    rooms[id] = {
+        id,
+        players: [{
+            id: socket.id,
+            name: socket.username || "Guest",
+            pos: -1,
+            stunned: false
+        }],
+        turnIndex: 0,
+        state: "LOBBY"
+    };
+
+    return rooms[id];
+}
+
+function joinRoom(room, socket) {
+    room.players.push({
+        id: socket.id,
+        name: socket.username || "Guest",
+        pos: -1,
+        stunned: false
+    });
+}
+
+module.exports = { rooms, createRoom, joinRoom };
